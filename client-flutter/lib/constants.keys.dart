ae10dd100b0cf74536c20a613478745e0b79eb1f
shubham tiwari <67504568+ShubhamTiwari479@users.noreply.github.com>
2023-03-20 09:07:21 +0530

    Delete constants.keys.dart

--- client-flutter/lib/constants.keys.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static const String RPC_URL = "http://127.0.0.1:7545";
-  static const String WS_URL = "ws://127.0.0.1:7545";
-  static const String PRIVATE_KEY =
-      "33c26c16a3f0496e11a37d79d4825139dca02ef87a175100b5b5bd7a6d56e53a";
-}
